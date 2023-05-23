97d65ec8622243548af9abad08057bc2152d9282
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-05-22 22:20:38 -0700

    Added sign-in

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import NavBar from './routes/navbar/navbar.component';
-
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>I am the shop page</h1>
@@ -14,6 +14,7 @@ const App = () => {
       <Route path='/' element={<NavBar />}>
         <Route index element={<Home />}/>
         <Route path='shop' element={<Shop />} />
+        <Route path='sign-in' element={<SignIn />} />
       </Route>
     </Routes>
   )
