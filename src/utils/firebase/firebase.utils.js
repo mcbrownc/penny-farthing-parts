5832e1e16717e554ae5906259d8aeedb5799f24e
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-06-05 22:28:02 -0700

    Added firebase util that creates new collection

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -43,4 +43,26 @@ export const createUserDocumentFromAuth = async (userAuth) => {
 
   const userSnapshot = await getDoc(userDocRef);
   console.log(userSnapshot);
-}
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email} = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt
+      });
+    } catch (error) {
+      console.log('error creating user', error.message)
+    }
+  }
+
+  return userDocRef;
+
+  //if user data does not exist
+  // create/set the document with the data from suerAuth in my collection
+  //if user data exists
+  //return userDocReg
+};
