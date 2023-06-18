a2a75c6aa1efc1735c96ac8f16f6074a7a5866dd
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-06-18 11:05:40 -0700

    Finished auth page

--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -34,10 +34,19 @@ const SignInForm = () => {
 
     try {
       const response = await signInAuthUserWithEmailAndPassword(email, password);
-      resetFormFields();
       console.log(response);
+      resetFormFields();
     } catch (error) {
-
+      switch (error.code) {
+        case 'auth/wrong-password':
+          alert('incorrect password for email');
+          break
+        case 'auth.user-not-found':
+          alert('no user associated with this email');
+          break
+        default:
+          console.log(error);
+      }
     }
   }
 
@@ -70,7 +79,7 @@ const SignInForm = () => {
         />
         <div className="buttons-container">
           <Button type="submit">Sign In</Button>
-          <Button buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle}>Google Sign In</Button>
         </div>
       </form>
     </div>
