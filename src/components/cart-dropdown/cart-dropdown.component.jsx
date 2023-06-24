540f2c37a3897682ed72c10cca918fe52793eb03
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-06-24 13:09:29 -0700

    Added link to checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,7 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { Link } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -9,12 +12,18 @@ import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const navigateToCheckoutHander = () => {
+    navigate('/checkout');
+  }
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
         {cartItems.map(item => <CartItem key={item.id} cartItem={item}/>)}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+        <Button onClick={navigateToCheckoutHander} >CHECKOUT</Button>
     </div>
   )
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+
+
+const Checkout = () => {
+  return (
+    <div>
+      <h1>Checkout</h1>
+    </div>
+  )
+};
+
+export default Checkout;
