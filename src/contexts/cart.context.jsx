613796736cf7e0d8eff9d395b8c6ba4738d1aa4a
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-06-23 23:16:31 -0700

    Fixed typos

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -4,7 +4,7 @@ const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find((cartItem) => cartItem.id === productToAdd.id)
 
   if (existingCartItem) {
-    return [cartItems.map((cartItem) => cartItem.id === productToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem)]
+    return cartItems.map((cartItem) => cartItem.id === productToAdd.id ? {...cartItem, quantity: cartItem.quantity + 1} : cartItem)
   }
   return [...cartItems, {...productToAdd, quantity: 1}];
 }
@@ -21,7 +21,7 @@ export const CartProvider = ({children}) => {
   const [cartItems, setCartItems] = useState([]);
 
   const addItemToCart = (productToAdd) => {
-    setCartItems(addCartItem(productToAdd));
+    setCartItems(addCartItem(cartItems, productToAdd));
   }
   const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
