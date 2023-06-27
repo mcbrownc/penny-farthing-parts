96d2640364f66f1ace1d4b924d969cb2425058c2
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-06-26 22:32:23 -0700

    Added total functionality

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,14 @@ import { CartContext } from '../../contexts/cart.context';
 import './cart-icon.styles.scss';
 
 const CartIcon = () => {
-const { isCartOpen, setIsCartOpen, cartTotal } = useContext(CartContext);
+const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
 
 const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
 
   return (
     <div className='cart-icon-container' onClick={toggleIsCartOpen}>
       <ShoppingIcon className='shopping-icon' />
-      <span className='item-count'>{cartTotal}</span>
+      <span className='item-count'>{cartCount}</span>
     </div>
   )
 };
--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -34,17 +34,27 @@ export const CartContext = createContext({
   addItemToCart: () => {},
   removeItemFromCart: () => {},
   clearItemFromCart: () => {},
+  cartCount: 0,
   cartTotal: 0,
 });
 
 export const CartProvider = ({children}) => {
   const [ isCartOpen, setIsCartOpen ] = useState(false);
   const [ cartItems, setCartItems ] = useState([]);
+  const [ cartCount, setCartCount ] = useState(0);
   const [ cartTotal, setCartTotal ] = useState(0);
+
+
+  useEffect(() => {
+    const newCartCount = cartItems.reduce((total, cartItem) => {
+      return total += cartItem.quantity
+    }, 0)
+    setCartCount(newCartCount);
+  }, [cartItems])
 
   useEffect(() => {
     const newCartTotal = cartItems.reduce((total, cartItem) => {
-      return total += cartItem.quantity
+      return total + cartItem.quantity * cartItem.price
     }, 0)
     setCartTotal(newCartTotal);
   }, [cartItems])
@@ -61,6 +71,6 @@ export const CartProvider = ({children}) => {
     setCartItems(clearCartItem(cartItems, cartItemToClear));
   }
 
-  const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartTotal };
+  const value = { isCartOpen, setIsCartOpen, addItemToCart, removeItemFromCart, clearItemFromCart, cartItems, cartCount, cartTotal };
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
