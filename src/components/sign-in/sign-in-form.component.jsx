b19fed998575d5f56986b10f8a9760c54d313e43
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-07-13 16:01:23 -0700

    Rest of components converted to styled

--- src/components/sign-in/sign-in-form.component.jsx
+++ src/components/sign-in/sign-in-form.component.jsx
@@ -9,7 +9,7 @@ import {
   signInAuthUserWithEmailAndPassword
 } from '../../utils/firebase/firebase.utils';
 
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 
 
@@ -57,7 +57,7 @@ const SignInForm = () => {
   }
 
   return (
-    <div className="sign-in-container">
+    <SignInContainer>
       <h2>Already have an account?</h2>
       <span>Sign in with your email or password</span>
       <form onSubmit={handleSubmit}>
@@ -77,12 +77,12 @@ const SignInForm = () => {
           value={password}
           required
         />
-        <div className="buttons-container">
+        <ButtonsContainer>
           <Button type="submit">Sign In</Button>
           <Button type='button' buttonType={BUTTON_TYPE_CLASSES.google} onClick={signInWithGoogle}>Google Sign In</Button>
-        </div>
+        </ButtonsContainer>
       </form>
-    </div>
+    </SignInContainer>
   )
 
 }
