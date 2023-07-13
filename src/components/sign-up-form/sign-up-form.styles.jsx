b0ea8e02b63c92eda6140ac70e8d8a157a600ba2
Corey McClendon-Brown <mcbrownc@hotmail.com>
2023-07-13 16:14:04 -0700

    Converted routes styling to styled-components

--- src/components/sign-up-form/sign-up-form.styles.jsx
+++ src/components/sign-up-form/sign-up-form.styles.jsx
@@ -9,13 +9,3 @@ export const SignUpContainer = styled.div`
     margin: 10px 0;
   }
 `;
-
-// .sign-up-container {
-//   display: flex;
-//   flex-direction: column;
-//   width: 380px;
-// }
-
-// h2 {
-//   margin: 10px 0;
-// }
