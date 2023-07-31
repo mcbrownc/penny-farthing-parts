70ef36b181cb3d1eee8e7f102c6f445a947d31ec
Corey <coreymcbrown@gmail.com>
2023-07-31 16:43:55 -0700

    Added cartReducer

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -38,6 +38,30 @@ export const CartContext = createContext({
   cartTotal: 0,
 });
 
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+  cartTotal: 0,
+}
+
+const cartReducer = (state, action) => {
+  const { type, paylaod } = action;
+
+  switch(type) {
+    case 'SET_CART_ITEMS':
+      return {
+        ...state,
+        ...payload
+      }
+    default:
+      throw new Error(`unhandled type of ${type} in cartReducer`)
+  }
+}
+
+
+
+
 export const CartProvider = ({children}) => {
   const [ isCartOpen, setIsCartOpen ] = useState(false);
   const [ cartItems, setCartItems ] = useState([]);
