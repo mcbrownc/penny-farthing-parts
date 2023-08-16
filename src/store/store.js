0843fb4e07d0f8576317dd4c9ea87fb12f28087d
Corey <coreymcbrown@gmail.com>
2023-08-15 17:53:51 -0700

    initialized store

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,8 @@
-import { compose, createStore, applyMiddlewar } from 'redux';
+import { compose, legacy_createStore as createStore, applyMiddlewar } from 'redux';
 import logger from 'redux-logger';
 
+import { rootReducer } from './root-reducer';
+
 // root reducer
+
+export const store = createStore(rootReducer)
