64dcdfa3e1c4a4354d84cfa2b8abad423abd555f
Corey <coreymcbrown@gmail.com>
2023-08-18 12:27:09 -0700

    Migrated categoires.context to redux

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,30 +1,33 @@
-import { useContext, useState, useEffect, Fragment } from 'react';
+import { useState, useEffect, Fragment } from 'react';
 import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux'; 
+
 import ProductCard from '../../components/product-card/product-card.component';
 
-import { CategoriesContext } from '../../contexts/categories.context';
+import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 import { CategoryContainer, CategoryTitle } from './category.styles';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
 
   useEffect(() => {
     setProducts(categoriesMap[category]);
-  }, [category, categoriesMap])
+  }, [category, categoriesMap]);
 
   return (
     <Fragment>
       <CategoryTitle>{category.toUpperCase()}</CategoryTitle>
       <CategoryContainer>
         {products &&
-          products.map((product) => <ProductCard key={product.id} product={product} />)
-        }
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
       </CategoryContainer>
     </Fragment>
-  )
-}
+  );
+};
 
 export default Category;
--- src/store/categories/category.reducer.js
+++ src/store/categories/category.reducer.js
@@ -1,4 +1,4 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types'
+import CATEGORIES_ACTION_TYPES from './category.types'
 
 export const CATEGORIES_INTITIAL_STATE = {
     categoriesMap: {}
@@ -8,7 +8,7 @@ export const categoriesReducer = (state = CATEGORIES_INTITIAL_STATE, action = {}
     const { type, payload }= action;
 
     switch(type) {
-        case 'CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP':
+        case CATEGORIES_ACTION_TYPES.SET_CATEGORIES_MAP:
             return { ...state, categoriesMap: payload }
         default:
             return state;
