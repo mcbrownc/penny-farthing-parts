e7250b91e7b434242255068b0e4252ad28df0d6e
Corey <coreymcbrown@gmail.com>
2023-08-18 17:04:00 -0700

    memoized with useSelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,7 +1,17 @@
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+    [selectCategoryReducer],
+    (categories) => categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
-      }, {});
-}
+    }, {})
+);
