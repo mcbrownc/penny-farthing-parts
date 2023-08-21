d40847152cbbb587079fbf9eb74edd9d49be07a9
Corey <coreymcbrown@gmail.com>
2023-08-21 16:59:35 -0700

    added user sagas

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import { useDispatch } from "react-redux";
 
 import { Routes, Route } from 'react-router-dom';
 
-import { onAuthStateChangeListener, createUserDocumentFromAuth, getCurrentUser } from "./utils/firebase/firebase.utils";
-
 import Home from './routes/home/home.component';
 import NavBar from './routes/navbar/navbar.component';
 import Authentication from './routes/sign-in/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from "./store/user/user.action";
+import { checkUserSession } from "./store/user/user.action";
 
 const App = () => {
   const dispatch = useDispatch();
   useEffect(() => {
-    getCurrentUser().then((user) => console.log(user))
+    dispatch(checkUserSession());
   }, [])
 
   return (
