85c1c73c264702ca532a86b049f41037890e83fb
Corey <coreymcbrown@gmail.com>
2023-08-22 16:36:50 -0700

    added payment-form component

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import PaymentForm from "../../components/payment-form/payment-form.component";
 
 import { CheckoutContainer, CheckoutHeader, HeaderBlock, Total} from './checkout.styles';
 
@@ -32,6 +33,7 @@ const Checkout = () => {
           cartItems.map((cartItem) => <CheckoutItem key={cartItem.id} cartItem={cartItem} />)
         }
         <Total>Total: ${cartTotal}</Total>
+        <PaymentForm />
     </CheckoutContainer>
   )
 };
