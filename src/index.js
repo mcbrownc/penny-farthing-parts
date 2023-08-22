403173bdce82e79daa820594af62e44213c75f47
Corey <coreymcbrown@gmail.com>
2023-08-22 16:03:09 -0700

    cleanup and stripe setup

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { stripePromise } from './stripe/stripe.utils';
 
 import App from './App';
 import { store, persistor } from './store/store';
 
-import reportWebVitals from './reportWebVitals';
-
 import './index.scss';
+import { Elements } from '@stripe/react-stripe-js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -18,14 +18,11 @@ root.render(
     <Provider loading={null} store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter>
+          <Elements stripe={stripePromise}>
             <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
