879ae405eb3a211e31bd925d12d0cfe535209afd
Corey <coreymcbrown@gmail.com>
2024-05-29 11:37:25 -0700

    knlkn

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ const App = () => {
     dispatch(checkUserSession());
   }, [])
 
-  // return (
-    // <Suspense jljfallback={<Spinner />}>
+  return (
+    <Suspense jljfallback={<Spinner />}>
       {/* <GlobalStyle /> */}
       <Routes>
         <Route path='/' element={<NavBar />}>
